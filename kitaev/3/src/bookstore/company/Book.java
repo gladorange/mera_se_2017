97333cad6e6f89695f07a60498328b13eb75c5d1
Vladimir Kitaev <vlkitaev@tieto.mera.ru>
2017-11-09 17:25:17 +0300

    Homework 3. Kitaev

---
+++ kitaev/3/src/bookstore/company/Book.java
@@ -0,0 +1,53 @@
+package bookstore.company;
+
+public class Book {
+    private String name, year;
+    private int numberOfPages;
+    private Author author;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Book() {
+        this.name = "";
+        this.year = "";
+        this.numberOfPages = 0;
+        this.author = new Author();
+    }
+
+    public Book(String name, String year, int numberOfPages, Author author) {
+        this.name = name;
+        this.year = year;
+        this.numberOfPages = numberOfPages;
+        this.author = author;
+    }
+}
