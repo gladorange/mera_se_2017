8a724ab4b12a2615e0d97029382515fdefd2bf6d
Yana Mishina <janekov7@gmail.com>
2017-11-14 22:17:44 +0300

    Add missing files to the third task

--- mishina/3/Author.java
+++ mishina/3/Author.java
@@ -1,2 +1,17 @@
-public class Autor {
+import java.util.ArrayList;
+
+public class Author extends Person {
+    ArrayList<Book> book = new ArrayList<>();
+
+    public Author(String firstName, String lastName, String birthDate, String country) {
+        super(firstName, lastName, birthDate, country);
+    }
+
+    public void addBook(Book book1) {
+       book.add(book1);
+    }
+
+    public ArrayList<Book> getBook() {
+        return book;
+    }
 }
--- mishina/3/Book.java
+++ mishina/3/Book.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Book {
+    String bookName;
+    String pubYear;
+    Author author;
+    int pages;
+
+    public Book(String bookName, String pubYear, Author author, int pages) {
+        this.bookName = bookName;
+        this.pubYear = pubYear;
+        this.author = author;
+        this.pages = pages;
+    }
+
 }
