68c7a1d6aaa8fb3c20ab001743dbde95eea0d8c5
shirmanova <dandanger@yandex.ru>
2017-11-16 21:52:58 +0300

    homework 3

--- shirmanova/3/BookStore.java
+++ shirmanova/3/BookStore.java
@@ -6,7 +6,8 @@ public class BookStore {
         Author[] authors = {pushkin, bulgakov, turgenev};
 
         Book captainDaughter = new Book("СaptainDaughter", 1836, 95, pushkin);
-        Book[] pushkinBook = {captainDaughter};
+        Book picovayaDama = new Book("PicovayaDama", 1834, 102, pushkin);
+        Book[] pushkinBook = {captainDaughter, picovayaDama};
 
         Book masterMargarita = new Book("MasterAndMargarita", 1940, 356, bulgakov);
         Book[] bulgakovBook = {masterMargarita};
@@ -18,8 +19,21 @@ public class BookStore {
         bulgakov.setBooks(bulgakovBook);
         turgenev.setBooks(tyrgenevBook);
 
+        System.out.println("Количество авторов: " + Person.getCount());
 
-        System.out.println(pushkin);
+        int totalPages = 0;
 
+        for (Author author : authors) {
+            Book[] books = author.getBooks();
+            int pages = 0;
+            for (Book b : books) {
+                pages = pages + b.getAmountOfPages();
+            }
+            totalPages = totalPages + pages;
+
+            System.out.println("Общее число страниц в книгах автора: " + author.getLastName() + " : " + pages);
+        }
+
+        System.out.println("Общее чисто страниц во всех книгах: " + totalPages);
     }
 }
