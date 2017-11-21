5f4ee2c7b50cbea30e2eb472cd79dd41054e865f
AlexKoltsov <alex-ask94@rambler.ru>
2017-11-21 23:17:41 +0300

    Alex Koltsov. Apply comment for home work 6

--- koltsov/5/bookstore/Author.java
+++ koltsov/5/bookstore/Author.java
@@ -4,16 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.StringTokenizer;
 
 public class Author extends Person {
     protected ArrayList<Book> books;
 
     public Author(String firstName, String secondName,
-                  String country, Date birthday,
+                  String country, LocalDate birthday,
                   ArrayList<Book> books) {
         super(firstName, secondName, country, birthday);
         this.books = books;
@@ -35,7 +36,7 @@ public class Author extends Person {
                 authorStr = br.readLine();
             }
 
-        } catch (IOException e) {
+        } catch (IOException | DateTimeParseException e) {
             e.printStackTrace();
             System.exit(-1);
         }
@@ -43,7 +44,7 @@ public class Author extends Person {
         return authors;
     }
 
-    private static Author createAuthorFromString(String authorStr) throws ParseException {
+    private static Author createAuthorFromString(String authorStr) throws ParseException, DateTimeParseException {
         StringTokenizer authorFields = new StringTokenizer(authorStr, "\t");
 
         ArrayList<String> fields = new ArrayList<>();
@@ -56,7 +57,7 @@ public class Author extends Person {
                     ". Переданная строка: " + authorStr, fields.size());
 
         Author author = new Author(fields.get(0), fields.get(1), fields.get(2),
-                new SimpleDateFormat("dd.MM.yyyy").parse(fields.get(3)),
+                LocalDate.parse(fields.get(3), DateTimeFormatter.ofPattern("d.M.yyyy")),
                 new ArrayList<>());
 
         return author;
--- koltsov/5/bookstore/Painter.java
+++ koltsov/5/bookstore/Painter.java
@@ -4,16 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.StringTokenizer;
 
 public class Painter extends Person {
     protected String drawingStyle;
     protected ArrayList<Artwork> artWorks;
 
-    public Painter(String firstName, String secondName, String country, Date birthday, String drawingStyle, ArrayList<Artwork> artWorks) {
+    public Painter(String firstName, String secondName, String country, LocalDate birthday, String drawingStyle, ArrayList<Artwork> artWorks) {
         super(firstName, secondName, country, birthday);
         this.drawingStyle = drawingStyle;
         this.artWorks = artWorks;
@@ -35,7 +36,7 @@ public class Painter extends Person {
                 painterStr = br.readLine();
             }
 
-        } catch (IOException e) {
+        } catch (IOException | DateTimeParseException e) {
             e.printStackTrace();
             System.exit(-1);
         }
@@ -43,7 +44,7 @@ public class Painter extends Person {
         return painters;
     }
 
-    private static Painter createPainterFromString(String painterStr) throws ParseException {
+    private static Painter createPainterFromString(String painterStr) throws ParseException, DateTimeParseException {
         StringTokenizer painterFields = new StringTokenizer(painterStr, "\t");
 
         ArrayList<String> fields = new ArrayList<>();
@@ -56,8 +57,7 @@ public class Painter extends Person {
                     ". Переданная строка: " + painterStr, fields.size());
 
         Painter painter = new Painter(fields.get(0), fields.get(1), fields.get(2),
-                new SimpleDateFormat("dd.MM.yyyy").parse(fields.get(3)),
-                fields.get(4), new ArrayList<>());
+                LocalDate.parse(fields.get(3), DateTimeFormatter.ofPattern("dd.MM.yyyy")), fields.get(4), new ArrayList<>());
 
         return painter;
     }
