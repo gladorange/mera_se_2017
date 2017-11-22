9dc729fa7246167090578fad3f6360e9a742be3c
shirmanova <dandanger@yandex.ru>
2017-11-22 21:20:23 +0300

    Homework 4

---
+++ shirmanova/3/Painter.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Painter extends Person {
+    List<Picture> pictures = new ArrayList<>();
+    private String style;
+
+    public Painter(String firstName, String lastName, int birthYear, String country, String style) {
+        super(firstName, lastName, birthYear, country);
+        this.style = style;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public List<Picture> getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(List<Picture> pictures) {
+        this.pictures = pictures;
+    }
+
+    @Override
+    public String toString() {
+        return "Painter{" +
+                "pictures=" + pictures +
+                ", style='" + style + '\'' +
+                '}';
+    }
+}
